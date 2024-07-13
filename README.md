966b872e334689e578fd758018db41017db9667c
Ames-hub <114128556+Ames-hub@users.noreply.github.com>
2024-07-13 23:45:33 +0800

    initial commit
    Please do note, I haven't tested this thoroughly. Its just a thing I made to help someone out

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# dlchannel bot
- a discord bot made using Hikari-Lightbulb to download ALL messages from a selected channel.
+# channel downloader bot
+A Discord bot that can be used to download all the messages from a channel.
+
+## Installation
+Installation is very simple.<br>
+Make sure you have python3.11 installed, and then copy the directory to the main.py file<br>
+(full path, starting at C:/ or whatever)
+
+Then run the following command in the terminal:
+```
+python3.11 -m venv venv
+```
+(IF that doesn't work, find the full path to your python executable and replace python3.11 with the full path)<br>
+
+This will create a virtual environment in the directory.<br>
+Then run the following command:
+```
+venv\Scripts\activate
+```
+Then run
+```
+pip install -r requirements.txt
+```
+Then finally to start the bot and run the setup script, run:
+```
+python main.py -O
+```
+
+## Usage
+The bot has 1 command.<br>
+"Download"<br>
+This command downloads all the messages from the last 5 years of the channel that the command is run in.<br>
+The messages are saved in 'downloaded/channel-*channel id here*'
+
+They are saved in the format of `user id - message id - timestamp: message content`<br>
---
+++ main.py
@@ -0,0 +1,57 @@
+import lightbulb
+import datetime
+import hikari
+import dotenv
+import os
+
+if os.path.exists('secrets.env'):
+    dotenv.load_dotenv('secrets.env')
+
+TOKEN = os.environ.get('TOKEN', None)
+ADMIN_UUID = os.environ.get('ADMIN_UUID', None)
+
+# Makes sure both are set and persistent.
+if TOKEN is None:
+    TOKEN = input("Please enter your bot token: ")
+    with open('secrets.env', 'w') as f:
+        f.write(f"TOKEN={TOKEN}\n")
+    print("Token saved to secrets.env")
+if ADMIN_UUID is None:
+    ADMIN_UUID = int(input("Please enter the discord user id that controls this bot: "))
+    with open('secrets.env', 'a') as f:
+        f.write(f"ADMIN_UUID={ADMIN_UUID}\n")
+    print("User ID saved to secrets.env")
+
+ADMIN_UUID = int(ADMIN_UUID)
+
+bot = lightbulb.BotApp(
+    token=TOKEN,
+    intents=hikari.Intents.ALL
+)
+
+@bot.command
+@lightbulb.app_command_permissions(dm_enabled=False)
+@lightbulb.add_cooldown(bucket=lightbulb.buckets.GuildBucket, length=10, uses=1)
+@lightbulb.command(name="download", description="Download the selected channel's messages and save them to a file.")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def download(ctx: lightbulb.SlashContext) -> None:
+    # Basic af permission check
+    if ctx.author.id != ADMIN_UUID:
+        return await ctx.respond("You don't have permission to use this command.")
+
+    await ctx.respond("Downloading channel messages...")
+
+    messages = await bot.rest.fetch_messages(
+        channel=ctx.channel_id,
+        before=datetime.datetime.now(),
+    )
+
+    file_dir = f"downloaded/channel-{ctx.channel_id}.txt"
+    os.makedirs('downloaded', exist_ok=True)
+    with open(file_dir, "w") as f:
+        for message in messages:
+            f.write(f"{message.author.id} - {message.id} - {message.timestamp}: {message.content}\n")
+
+    await ctx.edit_last_response(content=f"Channel messages downloaded and saved to {file_dir}")
+
+bot.run()
